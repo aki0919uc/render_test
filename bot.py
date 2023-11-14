dbbb18da45cacf4f5f7ab5eebfa6ae28967898e3
aki0919uc <150785484+aki0919uc@users.noreply.github.com>
2023-11-14 19:23:45 +0900

    bot.py

--- bot.py
+++ bot.py
@@ -1,53 +1,52 @@
-from flask import (
-    Flask, request, abort
-)
-
+from flask import Flask, request, abort
+import os
 from linebot import (
-    LineBotApi, WebhookHandler
+   LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+   InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
+   MessageEvent, TextMessage, TextSendMessage, FollowEvent,
+   ImageMessage, AudioMessage,
 )
-import os
 
 app = Flask(__name__)
-
-
-YOUR_CHANNEL_ACCESS_TOKEN = "zcDEWcocE7d3w1NwhyNWimXgqs+L67IpR3vZfrchPz53Pu9uCbN7mp6I1Dr3EjAxpT2t/II0v3C9DGkL6ACE9JPFlqB81oSSV1MfjlJ71Zv905MYnjqOisWBX7NesmTfi17JTFH5CEVrbuep5k+ziwdB04t89/1O/w1cDnyilFU="
-YOUR_CHANNEL_SECRET = "c46eba37b09568de62332b1bf5779a58"
-
+ #環境変数取得 
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+ #APIの設定 
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
-
+ #テスト用 
+@app.route("/")
+def hello_world():
+   return "hello world!"
+ #Webhookからのリクエストをチェック 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-
+   signature = request.headers['X-Line-Signature']
+   body = request.get_data(as_text=True)
+   app.logger.info("Request body: " + body)
+   try:
+       handler.handle(body, signature)
+   except InvalidSignatureError:
+       abort(400)
+   return 'OK'
+ #オウム返しプログラム 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text))
+   line_bot_api.reply_message(
+       event.reply_token,
+       TextSendMessage(text=event.message.text))
+ #友達追加時イベント 
+@handler.add(FollowEvent)
+def handle_follow(event):
+   line_bot_api.reply_message(
+       event.reply_token,
+       TextSendMessage(text='友達追加ありがとう'))
 
 
 if __name__ == "__main__":
-#    app.run()
-    port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+   port = int(os.getenv("PORT"))
+   app.run(host="0.0.0.0", port=port)
