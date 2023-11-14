5b8f539b0a9e7e6bd15b354301e37e78b126fec3
aki0919uc <150785484+aki0919uc@users.noreply.github.com>
2023-11-14 21:56:05 +0900

    bot.py

--- bot.py
+++ bot.py
@@ -28,8 +28,8 @@ def callback():
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
     # webhookのbodyを解析する
-　#この結果はadd関数で受け取る
-　#なお、Signatureが一致していない時はInvalidSignatureError例外が発生する
+    #この結果はadd関数で受け取る
+    #なお、Signatureが一致していない時はInvalidSignatureError例外が発生する
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
