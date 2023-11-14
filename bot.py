6ae24fdda77d3bf4bd7a7a5e6669d2ddf7808fcb
aki0919uc <150785484+aki0919uc@users.noreply.github.com>
2023-11-14 22:09:56 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,6 +35,7 @@ def callback():
     except InvalidSignatureError:
         abort(400)
     return 'OK'
+
 # MessageEvent
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
