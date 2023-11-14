445e6dfa22a3d2f77b3e27b10b9672bce17a9c93
aki0919uc <150785484+aki0919uc@users.noreply.github.com>
2023-11-14 21:58:02 +0900

    bot.py

--- bot.py
+++ bot.py
@@ -38,7 +38,7 @@ def callback():
 # MessageEvent
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-　　#テキストでの返信を行う
+    #テキストでの返信を行う
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=event.message.text )
