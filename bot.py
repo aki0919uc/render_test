5631b503acad932e78afead582ae3bd8fc0917c0
aki0919uc <150785484+aki0919uc@users.noreply.github.com>
2023-11-14 22:11:52 +0900

    bot.py

--- bot.py
+++ bot.py
@@ -1,48 +1,58 @@
-# ライブラリをインポート
 from flask import Flask, request, abort
-from linebot import (
-    LineBotApi, WebhookHandler
+
+from linebot.v3 import (
+    WebhookHandler
 )
-from linebot.exceptions import (
+from linebot.v3.exceptions import (
     InvalidSignatureError
 )
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage
+from linebot.v3.messaging import (
+    Configuration,
+    ApiClient,
+    MessagingApi,
+    ReplyMessageRequest,
+    TextMessage
 )
-import os
-#Flaskを準備
+from linebot.v3.webhooks import (
+    MessageEvent,
+    TextMessageContent
+)
+
 app = Flask(__name__)
-#環境変数からLINE Access Tokenを設定
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-#環境変数からLINE Channel Secretを設定
-LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
-#LineBotApiのインスタンスを生成
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-#WebhookHandlerのインスタンスを生成
-webhook_handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+configuration = Configuration(access_token='YOUR_CHANNEL_ACCESS_TOKEN')
+handler = WebhookHandler('YOUR_CHANNEL_SECRET')
+
+
 @app.route("/callback", methods=['POST'])
 def callback():
-    # HTTPリクエストヘッダからX-Line-Signatureを取り出す
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-    #テキストでpostされたデータを取得
+
+    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-    # webhookのbodyを解析する
-    #この結果はadd関数で受け取る
-    #なお、Signatureが一致していない時はInvalidSignatureError例外が発生する
+
+    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
+        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
+
     return 'OK'
 
-# MessageEvent
-@handler.add(MessageEvent, message=TextMessage)
+
+@handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
-    #テキストでの返信を行う
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text )
-    )
+    with ApiClient(configuration) as api_client:
+        line_bot_api = MessagingApi(api_client)
+        line_bot_api.reply_message_with_http_info(
+            ReplyMessageRequest(
+                reply_token=event.reply_token,
+                messages=[TextMessage(text=event.message.text)]
+            )
+        )
+
 if __name__ == "__main__":
     app.run()
