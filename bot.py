9ee82e2838c0b4e06ae5e00c627f9bdbe957ad6b
aki0919uc <150785484+aki0919uc@users.noreply.github.com>
2023-11-27 23:47:52 +0900

    bot.py

--- bot.py
+++ bot.py
@@ -1,48 +1,6 @@
-# ライブラリをインポート
-from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage
-)
-import os
-
-#Flaskを準備
+from flask import Flask
 app = Flask(__name__)
 
-#環境変数からLINE Access Tokenを設定
-LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
-#環境変数からLINE Channel Secretを設定
-LINE_CHANNEL_SECRET = os.environ["LINE_CHANNEL_SECRET"]
-
-#LineBotApiのインスタンスを生成
-line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
-#WebhookHandlerのインスタンスを生成
-webhook_handler = WebhookHandler(LINE_CHANNEL_SECRET)
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    # HTTPリクエストヘッダからX-Line-Signatureを取り出す
-    signature = request.headers['X-Line-Signature']
-
-    #テキストでpostされたデータを取得
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-
-    # webhookのbodyを解析する
-    #この結果はadd関数で受け取る
-    #なお、Signatureが一致していない時はInvalidSignatureError例外が発生する
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-if __name__ == "__main__":
-    app.run()
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
