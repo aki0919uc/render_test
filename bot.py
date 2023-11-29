e34c9872fb57d8c74fda25e396e968a2c69a1af1
aki0919uc <150785484+aki0919uc@users.noreply.github.com>
2023-11-29 23:28:36 +0900

    bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,8 @@
+import chromedriver_binary
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import time
 import os
 from flask import Flask, request, abort
 from linebot import (
@@ -11,6 +16,11 @@ from linebot.models import (
 )
 
 app = Flask(__name__)
+class UserContext:
+    waiting_for_number = False
+
+user_context = {}
+
 
 # LINE Developersのチャネルシークレットとチャネルアクセストークンを設定
 LINE_CHANNEL_ACCESS_TOKEN = os.getenv('LINE_CHANNEL_ACCESS_TOKEN')
@@ -39,9 +49,18 @@ def callback():
 def handle_message(event):
     # 受信したメッセージを取得
     received_text = event.message.text
-    if received_text == "車番検索"：
+    if received_text == "車番検索":
+        user_id = event.source.user_id
+        user_context[user_id] = UserContext()
+        user_context[user_id].waiting_for_number = True
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="検索する車番を入力してください：")
+        )
+
+    elif received_text.isdigit() and user_context.get(event.source.user_id) and user_context[event.source.user_id].waiting_for_number:
+        CarNum = received_text
         courseIDList = ["130","207","058","100","129","149","171","023","153","042","138","145","222","108","010","133","517","516","513","514","386","384","391","224","372","370","392","492","371","405","390","368","377","376","387","383","366","380","491","489","354","337","320","344","040","114","086","164","219","001","532","113","174","034","096","009","144","003","074","190","155","073","098","175","147","168","188","388","375","097","436","438","427","242","238","430","429","426","434","432","523","519","529","518","146","183","526","522","530","515","528","520","524","512","545","546","582","583","584","585"]
-        CarNum = input('input CarNumber:')
         options = Options()
         options.add_argument('--headless')
         driver = webdriver.Chrome(options=options)
@@ -59,16 +78,23 @@ def handle_message(event):
                     time.sleep(2)
                 route = driver.find_element(By.ID, "vehicle-overview-right").text.replace("\n", "")
                 if route != "現在走行しているバスはありません。":
-                    message = CarNum + 'を' + route + 'で検知しました' + "\n" + url
-                    payload = {'message': message}
-                    requests.post(LINEurl, headers=token_dic, data=payload,)
+                    reply_message = CarNum + 'を' + route + 'で検知しました' + "\n" + url
+                    b = 0
+                else:
+                    b = b+1 
         finally:
+            if b == 100:
+                reply_message = "検知しませんでした"
             driver.quit()
-        
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=reply_message)
-    )
+        )
+
+    else:
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="車番検索と入力してください：")
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
