845ff549657346820a84292ec4a4374e2f9c918b
aki0919uc <150785484+aki0919uc@users.noreply.github.com>
2023-12-01 20:04:07 +0900

    bot.py

--- bot.py
+++ bot.py
@@ -119,6 +119,7 @@ def callback():
 def handle_message(event):
     # 受信したメッセージを取得
     received_text = event.message.text
+    user_context[event.source.user_id] = UserContext()
     if received_text == "車番検索":
         user_context[event.source.user_id].waiting_for_number = False
         user_context[event.source.user_id].waiting_for_reset_number = False
@@ -129,9 +130,7 @@ def handle_message(event):
         )
         else:
             w.processing = True
-            user_id = event.source.user_id
-            user_context[user_id] = UserContext()
-            user_context[user_id].waiting_for_number = True
+            user_context[event.source.user_id].waiting_for_number = True
             line_bot_api.reply_message(
                 event.reply_token,
                 TextSendMessage(text="検索する車番を入力してください：")
