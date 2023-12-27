e53ce18d15f54b5bddd63ee786b1b6b4b106e471
aki0919uc <150785484+aki0919uc@users.noreply.github.com>
2023-12-27 21:03:07 +0900

    bot.py

--- bot.py
+++ bot.py
@@ -46,6 +46,7 @@ class dettime:
     dettime1552 = "03:00"
 d = dettime()
 courseIDList = ["130","207","058","100","129","149","171","023","153","042","138","145","222","108","010","133","517","516","513","514","386","384","391","224","372","370","392","492","371","405","390","368","377","376","387","383","366","380","491","489","354","337","320","344","040","114","086","164","219","001","532","113","174","034","096","009","144","003","074","190","155","073","098","175","147","168","188","388","375","097","436","438","427","242","238","430","429","426","434","432","523","519","529","518","146","183","526","522","530","515","528","520","524","512","545","546","582","583","584","585"]
+courseNameList = [""]
 
 
 def extract_js_var(soup, js_var):
@@ -65,7 +66,6 @@ def log():
     token_dic = {'Authorization': 'Bearer ' + access_token}
 
 
-    j = 2
     for a in range(0,len(courseIDList)):
         url_locate = 'https://transfer.navitime.biz/sotetsu/smart/location/BusLocationMap?courseId=0003400'+ courseIDList[a]
         res = requests.get(url_locate)
@@ -79,17 +79,16 @@ def log():
             url_navi_loc = 'https://transfer.navitime.biz/sotetsu-style-contents/bus-location/stops?courseId=0003400' + courseIDList[a] + '&vehicleId=' + str(CarNum)
             if CarNum == 1551:
                 if td1551 > td or td1551 < timezero:
-                    message1551 = '1551が動いています。' + str(j) +'列目です\n' + url_navi_loc
+                    message1551 = '1551が動いています。' + str(a) +'列目です\n' + url_navi_loc
                     payload1551 = {'message': message1551}
                     requests.post(LINEurl, headers=token_dic, data=payload1551,)
                 d.dettime1551 = now
             if CarNum == 1552:
                 if td1552 > td or td1552 < timezero:
-                    message1552 = '1552が動いています。' + str(j) +'列目です\n' + url_navi_loc
+                    message1552 = '1552が動いています。' + str(a) +'列目です\n' + url_navi_loc
                     payload1552 = {'message': message1552}
                     requests.post(LINEurl, headers=token_dic, data=payload1552,)
                 d.dettime1552 = now
-            j = j+1
             time.sleep(0.3)
 
 @app.route('/notify', methods=['HEAD','GET'])
@@ -141,25 +140,32 @@ def handle_message(event):
     elif received_text.isdigit() and user_context.get(event.source.user_id) and user_context[event.source.user_id].waiting_for_number:
         user_context[event.source.user_id].waiting_for_number = False
         b = 0
+        c = 1
+        e = 5
+        #b:不検知判定用、c:courseID遷移用、e:読み込み中検知予防用
         CarNum = received_text
         options = Options()
         options.add_argument('--headless')
         driver = webdriver.Chrome(options=options)
 
         try:
-            for c in range(0,len(courseIDList)):
+            while c <= len(courseIDList):
                 url_navi = 'https://transfer.navitime.biz/sotetsu-style-contents/bus-location/stops?courseId=0003400' + courseIDList[c] + '&vehicleId=' + CarNum
                 driver.get(url_navi)
-                if c == 1:
-                    time.sleep(5)
-                else:
-                    time.sleep(2)
+                time.sleep(e)
                 route = driver.find_element(By.ID, "vehicle-overview-right").text.replace("\n", "")
-                if route != "現在走行しているバスはありません。":
+                if route == "読み込み中…":
+                    e = e+2
+                elif route != "現在走行しているバスはありません。":
                     reply_message = CarNum + 'を' + route + 'で検知しました' + "\n" + url_navi
                     b = 0
+                    c = c+1
+                    e = 2
                 else:
-                    b = b+1 
+                    b = b+1
+                    c = c+1
+                    e = 2
+
         finally:
             if b == 100:
                 reply_message = "検知しませんでした"
