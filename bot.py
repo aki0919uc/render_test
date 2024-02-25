3370f93eb1fd081e8733237b7cbe6264dc499913
aki0919uc <150785484+aki0919uc@users.noreply.github.com>
2024-02-25 22:21:19 +0900

    bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import requests
 import re
 import json
 import time
+import openpyxl
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -39,12 +40,21 @@ line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 LINEurl = "https://notify-api.line.me/api/notify"
 
+excel_path = '/Users/aki_u/documents/Python/buslocate.xlsx'
+wb = openpyxl.load_workbook(excel_path)
+ws = wb['Sheet1']
+wb.save(excel_path)
+wb.close
 
 class dettime:
     dettime1361 = "2024/02/25-03:00"
     dettime1362 = "2024/02/25-03:00"
 d = dettime()
+
 courseIDListAyase = ["041","231","177","217","170","212","131","092","102","140","202","054","064","166","105","148","112","180","110","085","248","247","046","121","019","039","135","199","070","152","002","089","107","410","409","049","136","211","167","078","060","141","057","082","127","214"]
+courseIDListAsahi = ["130","207","058","100","129","149","171","023","153","042","138","145","222","108","010","133","517","516","513","514","000","386","384","391","224","372","370","392","000","492","371","405","390","368","377","376","387","383","366","380","491","489","000","354","337","320","344","040","114","086","164","219","000","001","532","113","174","034","096","009","144","003","074","190","155","073","098","175","147","168","188","000","388","375","097","436","438","427","242","238","430","429","426","434","432","000","523","519","529","518","146","183","526","522","530","515","528","520","524","512","545","546","000","582","583","584","585"]
+
+
 
 def extract_js_var(soup, js_var):
     script = soup.find('script', string=re.compile(js_var, re.DOTALL))    #scriptタグを検索
@@ -58,8 +68,21 @@ def extract_js_var(soup, js_var):
 def log():
     FMT = "%Y/%m/%d-%H:%M"
     now = datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))).strftime(FMT)
+    FMTtime = "%H:%M"
+    nowtime = datetime.datetime.now().strftime(FMTtime)
     td = datetime.timedelta(hours=1, minutes=30)
     token_dic = {'Authorization': 'Bearer ' + access_token}
+    MaxRow = ws.max_row +1
+    ws.cell(MaxRow,1,value = nowtime)
+    ws.cell(MaxRow,22,value = nowtime)
+    ws.cell(MaxRow,44,value = nowtime)
+    ws.cell(MaxRow,54,value = nowtime)
+    ws.cell(MaxRow,73,value = nowtime)
+    ws.cell(MaxRow,87,value = nowtime)
+    ws.cell(MaxRow,104,value = nowtime)
+
+
+
 
 
     for a in range(0,len(courseIDListAyase)):
@@ -72,6 +95,7 @@ def log():
             td1361 = datetime.datetime.strptime(now,FMT) - datetime.datetime.strptime(d.dettime1361,FMT)
             td1362 = datetime.datetime.strptime(now,FMT) - datetime.datetime.strptime(d.dettime1362,FMT)
             CarNum = extract_js_var(soup, 'busPin')
+            ws.cell(MaxRow,a,value = CarNum)
             url_navi_loc = 'https://transfer.navitime.biz/sotetsu-style-contents/bus-location/stops?courseId=0003400' + courseIDListAyase[a] + '&vehicleId=' + str(CarNum)
             if CarNum == 1361:
                 if td1361 > td:
@@ -86,6 +110,9 @@ def log():
                     requests.post(LINEurl, headers=token_dic, data=payload1362,)
                 d.dettime1362 = now
             time.sleep(0.3)
+    wb.save(excel_path)
+
+
 
 @app.route('/notify', methods=['HEAD','GET'])
 def notify():
